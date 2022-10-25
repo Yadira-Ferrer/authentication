55cddd62559545b3ea291c05b8b23e13d9de7ef2
yadiraferrer <yyfm.95@gmail.com>
2022-10-24 23:33:42 -0600

    Setup themes

--- auth-app/src/app/app.component.ts
+++ auth-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'auth-app';
+  currentTheme = 'arya-blue';
+
+  constructor(private themeService: ThemeService) {}
+
+  changeTheme(theme: string) {
+    this.themeService.switchTheme(theme);
+  }
 }
