dc7eebfa92e51d87a605b30430ed9922553190d4
yadiraferrer <yyfm.95@gmail.com>
2022-10-28 18:12:20 -0600

    Add social buttons component

--- auth-app/src/app/auth/auth.module.ts
+++ auth-app/src/app/auth/auth.module.ts
@@ -8,9 +8,10 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { MainComponent } from './pages/main/main.component';
 import { PrimengModule } from '../primeng/primeng.module';
+import { SocialButtonsComponent } from './components/social-buttons/social-buttons.component';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, MainComponent],
+  declarations: [LoginComponent, RegisterComponent, MainComponent, SocialButtonsComponent],
   imports: [CommonModule, AuthRoutingModule, FormsModule, PrimengModule],
 })
 export class AuthModule {}
